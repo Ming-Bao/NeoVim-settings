cb55bc9bbb89d6f318d595927bf8752c34f7ddf8
Ming <mingbao2891@gmail.com>
2024-05-02 14:22:30 +1200

    Added half page jump, curser stays in middle keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+--Leader pv to open file explore mode
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--Half page jumps center curser at the center of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
